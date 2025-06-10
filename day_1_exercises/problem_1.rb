89123b2f282f2539b53b207fd219e6e4cda01853
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-10 14:20:47 +0800

    Print name and age

---
+++ day_1_exercises/problem_1.rb
@@ -0,0 +1,11 @@
+# Print your name and age using variables.
+
+def print_name_age(name, age)
+  puts name
+  puts age
+end
+
+given_name = "Renz"
+given_age = 22
+
+print_name_age(given_name, given_age)
