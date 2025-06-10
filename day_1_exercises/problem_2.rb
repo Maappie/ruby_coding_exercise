9e39f8eb7f3c77a574fff2352590ff51985ee936
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-10 17:19:32 +0800

    problem_2

---
+++ day_1_exercises/problem_2.rb
@@ -0,0 +1,7 @@
+#Create two variables and concatenate like your first name and a last name.
+
+def print_full_name(firstName, lastName)
+  puts firstName + " " + lastName
+end
+
+print_full_name("Renz Daneco", "Mapa")
