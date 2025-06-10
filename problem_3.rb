b1883592cb8aa2d1c1e25e039feca760ac5a7b7e
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-10 18:04:05 +0800

    problem_3

---
+++ problem_3.rb
@@ -0,0 +1,11 @@
+# Create a variable for your favorite number, double it, and print the result.
+
+def double_number(number)
+  number * 2
+end
+
+favorite_number = 10
+
+double = double_number(favorite_number)
+
+puts double
