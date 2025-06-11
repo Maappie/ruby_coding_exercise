59619563b54217ac2346cc02477826c327ce184a
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-11 15:34:20 +0800

    Start with the number 50, add 30 to it using the += operator, and print the new value.

---
+++ day_1_exercises/problem_42.rb
@@ -0,0 +1,10 @@
+# Start with the number 50, add 30 to it using the += operator, and print the new value.
+
+def numberAdder
+  number = 50
+  number += 30
+  
+  puts "New value is #{number}"
+end
+
+numberAdder
