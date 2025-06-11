caabd469d3c88fde507d5a5d19d5bec22a2be782
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-11 15:36:21 +0800

    Begin with the number 100, subtract 25 from it using the -= operator, and display the result.

---
+++ day_1_exercises/problem_43.rb
@@ -0,0 +1,10 @@
+# Begin with the number 100, subtract 25 from it using the -= operator, and display the result.
+
+def numberSubtractor
+  number = 100
+  number -= 25
+
+  puts "The value is #{number}"
+end
+
+numberSubtractor
