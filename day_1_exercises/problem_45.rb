9e3beb1d11964f0f26dba93f41ffb1bc2dd72a64
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-11 15:38:24 +0800

    Start with 64, divide it by 8 using the /= operator, and print the final result.

---
+++ day_1_exercises/problem_45.rb
@@ -0,0 +1,10 @@
+# Start with 64, divide it by 8 using the /= operator, and print the final result.
+
+def numberDivider
+  number = 64
+  number /= 8
+
+  puts "The value is #{number}"
+end
+
+numberDivider
