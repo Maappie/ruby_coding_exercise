a8922e8efe5a6634ba934b9ee6eed1366b4b32f0
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-11 14:52:56 +0800

    Create a variable for the price of two items, calculate the total, and print it with a message like “The total is [total].

---
+++ day_1_exercises/problem_27.rb
@@ -0,0 +1,8 @@
+# Create a variable for the price of two items, calculate the total, and print it with a message like “The total is [total].”
+
+def itemsPrice(item_one, item_two)
+  puts "The total is #{item_one + item_two}"
+end
+
+itemsPrice(50.56, 10.11)
+
