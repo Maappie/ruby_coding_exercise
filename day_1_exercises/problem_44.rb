dfc1db00b94e545943e052b30ccb859001632ba3
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-11 15:37:19 +0800

    Using the number 8, multiply it by 5 with the *= operator, then print the result.

---
+++ day_1_exercises/problem_44.rb
@@ -0,0 +1,10 @@
+# Using the number 8, multiply it by 5 with the *= operator, then print the result.
+
+def numberMultiplier
+  number = 8
+  number *= 5
+
+  puts "The value is #{number}"
+end
+
+numberMultiplier
