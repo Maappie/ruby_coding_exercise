2e43b61e1da0261ed340701489c7d2495c6a1408
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 08:30:08 +0800

    add fruit name in an array list of fruits

---
+++ day_2_exercises/problem_1.rb
@@ -0,0 +1,12 @@
+# You have a list of fruits, Add the fruit "orange" to the list and print the updated list.
+#["apple", "banana", "cherry", "date"]
+
+def fruitsList(fruits)
+  fruits << "orange"
+end
+
+  fruits = ["apple","banana","cherry","date"]
+
+  fruitsList(fruits)
+
+  puts fruits
