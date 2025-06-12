86180e6117be657098ccad005822e2f81879c607
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 21:02:55 +0800

    # Create a hash to store information about your favorite movie: title, director, year, and rating. Print the entire hash.

---
+++ day_2_exercises/problem_28.rb
@@ -0,0 +1,10 @@
+# Create a hash to store information about your favorite movie: title, director, year, and rating. Print the entire hash.
+
+movie = {
+  title: "Dragon",
+  director: "Uunknown",
+  year: "1999",
+  raring: "10/10"
+}
+
+puts movie
