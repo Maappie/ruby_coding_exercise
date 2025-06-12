c1bc3923a1bbc4f49763faaf59456010f9364775
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 09:02:16 +0800

    Create an array that contains the days of the week, from Monday to Sunday. Print the third day in the array.

---
+++ day_2_exercises/problem_3.rb
@@ -0,0 +1,9 @@
+# Create an array that contains the days of the week, from Monday to Sunday. Print the third day in the array.
+
+def printThirdDay(week)
+  puts week[2]
+end
+
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+printThirdDay(week)
