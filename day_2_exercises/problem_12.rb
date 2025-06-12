f40edc83dd8013e673cc4df2a757f89b77df7497
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 20:58:26 +0800

    # Given an array of numbers , reverse the array and print the result. #[3, 6, 9, 12, 15]

---
+++ day_2_exercises/problem_12.rb
@@ -0,0 +1,10 @@
+# Given an array of numbers , reverse the array and print the result.
+#[3, 6, 9, 12, 15]
+
+def reverseArr(arrNum)
+  arrNum.reverse
+end
+
+arrNum = [3, 6, 9, 12, 15]
+
+puts reverseArr(arrNum)
