cfcbb21a6f740e124511f19ec728c4bb7f639109
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 21:01:55 +0800

    # Create a hash with three of your favorite books and their authors. Print the author of the second book.

---
+++ day_2_exercises/problem_23.rb
@@ -0,0 +1,5 @@
+# Create a hash with three of your favorite books and their authors. Print the author of the second book.
+
+favorite_book = { bookOne: "authorOne", bookTwo: "authorTwo", bookThree: "authorThree"}
+
+puts favorite_book[:bookTwo]
