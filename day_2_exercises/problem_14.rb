7408450e4bdc446c9455738ea9cc0c6be286300e
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 20:59:00 +0800

    # You have two arrays: ["red", "green", "blue"] and ["yellow", "purple", "pink"]. Combine them into one array and print the result.

---
+++ day_2_exercises/problem_14.rb
@@ -0,0 +1,10 @@
+# You have two arrays: ["red", "green", "blue"] and ["yellow", "purple", "pink"]. Combine them into one array and print the result.
+
+def printTwoarr(color_one, color_two)
+  puts "#{color_one + color_two}"
+end
+
+color_one = ["red", "green", "blue"]
+color_two = ["yellow", "purple", "pink"]
+
+printTwoarr(color_one, color_two)
