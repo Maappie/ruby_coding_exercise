0aafb77fc7638d7b39e3884596faa588de749f87
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 20:31:44 +0800

    # Create an array of your 5 favorite movies. Print the length of the array.

---
+++ day_2_exercises/problem_7.rb
@@ -0,0 +1,10 @@
+# Create an array of your 5 favorite movies. Print the length of the array.
+
+
+def lengthPrint(movies)
+  movies.length
+end
+
+movies = ["Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5"]
+
+puts lengthPrint(movies)
