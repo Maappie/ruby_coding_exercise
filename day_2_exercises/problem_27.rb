871235ee2f5efff11c2acfaab399def559d7b716
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-12 21:02:44 +0800

    # Print all the values in the hash. # { red: "#FF0000", green: "#00FF00", blue: "#0000FF" }

---
+++ day_2_exercises/problem_27.rb
@@ -0,0 +1,7 @@
+# Print all the values in the hash.
+# { red: "#FF0000", green: "#00FF00", blue: "#0000FF" }
+
+
+colors = { red: "#FF0000", green: "#00FF00", blue: "#0000FF" }
+
+puts colors.values
