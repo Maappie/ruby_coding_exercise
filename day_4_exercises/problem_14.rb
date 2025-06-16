470eec0ccd0c61a9926eb1d650fc57088bb2b1ec
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-17 01:19:39 +0800

    # Use the times method to simulate rolling two dice 10 times. After each roll, print both numbers and whether their sum is odd or even.

---
+++ day_4_exercises/problem_14.rb
@@ -0,0 +1,3 @@
+# Create a Ruby program that generates a random string of 8 characters, 
+# where each character is randomly chosen from a set of lowercase letters a to z (Array), using the times method.
+
