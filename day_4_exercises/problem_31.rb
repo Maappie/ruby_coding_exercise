d17009a00a9ad3a44bc8030ac5c5e0f5762e24d8
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-17 01:22:18 +0800

    # Use a for loop to calculate and print the sum of all numbers from 1 to 100.

---
+++ day_4_exercises/problem_31.rb
@@ -0,0 +1,14 @@
+# Use a for loop to calculate and print the sum of all numbers from 1 to 100.
+
+
+def sumNumbers
+
+  sum = 0
+
+  for i in 1..100
+    sum += i
+  end
+  puts sum
+end
+
+sumNumbers
