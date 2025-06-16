f55641962c6d1cc8e2941f2224097c679b5be3a4
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-17 01:24:08 +0800

    # solve the following using while # Write a program that counts down from 10 to 1 and prints each number.

---
+++ day_4_exercises/problem_43.rb
@@ -0,0 +1,12 @@
+# solve the following using while
+# Write a program that counts down from 10 to 1 and prints each number.
+
+def countNumbers
+  count = 10
+  while 1 <= count
+    puts count
+    count -= 1
+  end
+end
+puts "Numbers: "
+countNumbers
