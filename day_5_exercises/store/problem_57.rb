c7356ca272fd3e5aa0d1376b9729c01ce3546eed
Renz Daneco Mapa <renzmapa0321@gmail.com>
2025-06-19 20:09:19 +0800

    # Print the name of all the products

---
+++ day_5_exercises/store/problem_57.rb
@@ -0,0 +1,13 @@
+# Print the name of all the products
+
+
+require_relative 'config/environment'
+
+def printProduct_names
+  puts "Product names list:"
+  Product.pluck(:name).each_with_index do |target, i|
+    puts "#{i + 1}. #{target}."
+  end
+end
+
+printProduct_names
